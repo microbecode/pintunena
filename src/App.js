267f111b5e4f84cd776579e756af95b72d219627
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-01-05 18:04:07 +0200

    Minor edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <div>This is the official website of SIA Pintunena.</div>
         <marquee>We do serious business!</marquee>
         <img src="https://c.tenor.com/X26Gz2f4i9YAAAAM/cat-angry.gif" alt="cat"></img>
-        <div>Company data: <a href="https://www.ur.gov.lv/en/legal-entity/?id=50203308681">https://www.ur.gov.lv/en/legal-entity/?id=50203308681</a></div>
         <div>Contact information: <a href="https://www.linkedin.com/in/lauri-peltonen/">LinkedIn</a></div>
         
       </header>

60974cba6737147b1a2e90656e12534d1618e758
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2022-01-05 18:02:51 +0200

    added marquee

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-distracting-elements */
 import './App.css';
 
 function App() {
@@ -5,10 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>This is the official website of SIA Pintunena.</div>
-        <div>We do serious business!</div>
+        <marquee>We do serious business!</marquee>
         <img src="https://c.tenor.com/X26Gz2f4i9YAAAAM/cat-angry.gif" alt="cat"></img>
         <div>Company data: <a href="https://www.ur.gov.lv/en/legal-entity/?id=50203308681">https://www.ur.gov.lv/en/legal-entity/?id=50203308681</a></div>
         <div>Contact information: <a href="https://www.linkedin.com/in/lauri-peltonen/">LinkedIn</a></div>
+        
       </header>
     </div>
   );
